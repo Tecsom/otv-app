2e6e1335cde376cc6e15efcf7eaa641793fed249
GAMBSTER <gamboa040200@gmail.com>
2024-04-17 13:21:31 -0600

    added env config to supabase

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,10 +1,8 @@
+import 'dotenv/config';
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = () => {
-  return createClient(
-    'https://ysbttwpoacgzbospykzx.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzYnR0d3BvYWNnemJvc3B5a3p4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMjc4MTAxNywiZXhwIjoyMDI4MzU3MDE3fQ.ni_jrH8v7N6NXjKGTvDlUdqdN59KwQJiKsonduLJ2FY'
-  );
+  return createClient(process.env.SUPABASE_URL ?? '', process.env.SUPABASE_ANON_KEY ?? '');
 };
 
 export default supabase;
