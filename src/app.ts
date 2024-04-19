a7d40d5307a82aa5e11c9de44e952dc8f60abc75
GAMBSTER <gamboa040200@gmail.com>
2024-04-19 13:33:54 -0600

    added electron

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import { AddressInfo } from 'net';
+import { BrowserWindow, app } from 'electron';
 import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
@@ -24,6 +26,47 @@ appExpress.use('/', viewsRoutes);
 // appExpress.use('/api', apiRoutes);
 
 const PORT = 3000;
-appExpress.listen(PORT, async () => {
+const server = appExpress.listen(PORT, async () => {
   console.log('Server up running in:', PORT);
+});
+
+declare global {
+  var globalWindow: any;
+}
+
+function createWindow() {
+  // Create the browser window.
+  console.log('createWindow');
+  const mainWindow = new BrowserWindow({
+    autoHideMenuBar: true,
+    height: 600,
+    width: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      preload: path.join(__dirname, 'preload.js')
+      //devTools: !app.isPackaged,
+    },
+    useContentSize: true,
+    show: false,
+    icon: path.resolve(__dirname, './assets/img/', 'logo_tpj.ico')
+  });
+
+  const { port } = server.address() as AddressInfo;
+  const url = `http://localhost:${port}/`;
+
+  mainWindow.loadURL(url);
+
+  mainWindow.show();
+
+  globalThis.globalWindow = mainWindow;
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
