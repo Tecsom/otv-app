b5b59d0ad1120fab7214f66f5b4411cf6367ed52
Juan Soto M <tecsom.mx@gmail.com>
2024-04-22 14:16:52 -0600

    v

--- src/routes/views.ts
+++ src/routes/views.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { renderClientsPage, renderHomePage, renderLoginPage } from '../controllers/views';
+import { renderClientsPage, renderHomePage, renderLoginPage, renderCatalogoInterno } from '../controllers/views';
 
 const router = express.Router();
 
 router.get('/', renderHomePage);
 router.get('/login', renderLoginPage);
 router.get('/clientes', renderClientsPage);
+router.get('/catalogo/interno', renderCatalogoInterno)
 
 export default router;
