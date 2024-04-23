15dd5669648865ae37c99afa04c0afe17bbfbb50
Juan Soto M <tecsom.mx@gmail.com>
2024-04-22 18:09:13 -0600

    json unidades

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,6 @@
 import ViewsRoutes from './views';
 import ScannerRoutes from './scanner';
 import ClientesRoutes from './clientes';
-import unidades_medida from './unidades_medida';
 
 export const viewsRoutes = [ViewsRoutes];
-export const apiRoutes = [ScannerRoutes, ClientesRoutes, unidades_medida];
+export const apiRoutes = [ScannerRoutes, ClientesRoutes];
--- src/types/unidades_medida.ts
+++
@@ -1,6 +0,0 @@
-export interface UnidadMedida {
-    id: number,
-    created_at: number
-    name: string
-    short_name: string
-}
