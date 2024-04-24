9d215441420d8ab954c27bc75d652d33b2b1400a
Salvador Figuerola <ing.figuerola@tecsomx.com>
2024-04-24 12:14:32 -0600

    avances layout verificador

--- src/controllers/views.ts
+++ src/controllers/views.ts
@@ -35,6 +35,6 @@ export const renderOrdenesCompra = async (req: Request, res: Response) => {
   }
 };
 
-export const renderVerificador = async (_: Request, res: Response) => res.render('verificador.ejs');
-export const renderVerificadorEmbarques = async (_: Request, res: Response) => res.render('verificador-embarques.ejs');
-export const renderVerificadorOrdenes = async (_: Request, res: Response) => res.render('verificador-ordenes.ejs');
+export const renderVerificador = async (_: Request, res: Response) => res.render('verificador.ejs', { layout: "./../layouts/horizontal.ejs"});
+export const renderVerificadorEmbarques = async (_: Request, res: Response) => res.render('verificador-embarques.ejs', { layout: "./../layouts/horizontal.ejs"});
+export const renderVerificadorOrdenes = async (_: Request, res: Response) => res.render('verificador-ordenes.ejs', { layout: "./../layouts/horizontal.ejs"});
