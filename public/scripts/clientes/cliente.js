6c0f5ccb2c17da7b7b6d9cbca888827c4d742fba
Salvador Figuerola <ing.figuerola@tecsomx.com>
2024-04-30 13:34:08 -0600

    fix preview dropzone

--- public/scripts/clientes/cliente.js
+++ public/scripts/clientes/cliente.js
@@ -1,11 +1,19 @@
 import { fetchData, loadingButton } from '/public/scripts/helpers.js';
 const previewTemplate = `<div class="dz-preview dz-file-preview">
 <div class="dz-details">
+  <div class="dz-thumbnail">
+    <img data-dz-thumbnail>
+    <span class="dz-nopreview">No preview</span>
+    <div class="dz-success-mark"></div>
+    <div class="dz-error-mark"></div>
+    <div class="dz-error-message"><span data-dz-errormessage></span></div>
     <div class="progress">
       <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuemin="0" aria-valuemax="100" data-dz-uploadprogress></div>
     </div>
   </div>
+</div>
 </div>`;
+
 const dropzoneCliente = new Dropzone('#dpz-cliente', {
   previewTemplate: previewTemplate,
   parallelUploads: 1,
