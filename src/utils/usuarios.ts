b759a7e2e31420b4ebb4304877d8fd2f9a87247f
GAMBSTER <gamboa040200@gmail.com>
2024-05-01 13:33:05 -0600

    changed table name

--- src/utils/usuarios.ts
+++ src/utils/usuarios.ts
@@ -2,7 +2,7 @@ import supabase from '@/config/supabase';
 import { Usuario } from '@/types/usuarios';
 
 export const getUsuarios = async (): Promise<Usuario[]> => {
-  const { data, error } = await supabase().from('users').select('*');
+  const { data, error } = await supabase().from('usuarios').select('*');
   if (error) throw error;
   return data;
 };
