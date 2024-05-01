c8abe3077b36cde3a848c9fadc14131d6041b516
GAMBSTER <gamboa040200@gmail.com>
2024-05-01 13:15:22 -0600

    added table

---
+++ public/scripts/usuarios/usuariosTable.js
@@ -0,0 +1,24 @@
+const options = `
+  <button class="btn btn-sm btn-primary" data-toggle="modal" data-target="#editUsuarioModal">
+    <i class="fas fa-edit"></i>
+    </button>
+    <button class="btn btn-sm btn-danger" data-toggle="modal" data-target="#deleteUsuarioModal">
+    <i class="fas fa-trash"></i>
+    </button>`;
+
+usuariosTable = $('#usuarios-table').DataTable({
+  columns: [
+    { data: 'name', title: 'Nombre', defaultContent: '' },
+    { data: 'email', title: 'Correo' },
+    { data: 'status', title: 'Estado', defaultContent: 'Activo' },
+    { data: 'role', title: 'Rol', defaultContent: '' },
+    { data: 'action', title: 'Acciones', orderable: false, searchable: false, defaultContent: options }
+  ],
+  searching: false,
+  ordering: false,
+  info: false,
+  dom: 'rtp',
+  language: {
+    url: 'https://cdn.datatables.net/plug-ins/1.13.7/i18n/es-ES.json'
+  }
+});
