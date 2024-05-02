343f7fc5f97ab9a369afcd4a78f0dd315cf14d19
Gambster <gamboa040200@gmail.com>
2024-05-02 11:49:18 -0600

    instaler windows

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = () => {
-  return createClient(process.env.SUPABASE_URL ?? '', process.env.SUPABASE_ANON_KEY ?? '');
+  return createClient( 'https://ysbttwpoacgzbospykzx.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzYnR0d3BvYWNnemJvc3B5a3p4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMjc4MTAxNywiZXhwIjoyMDI4MzU3MDE3fQ.ni_jrH8v7N6NXjKGTvDlUdqdN59KwQJiKsonduLJ2FY');
 };
 
 export default supabase;
