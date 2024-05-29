449a2c801a0cf8fd985a8efd1830bd3cde9c5805
GAMBSTER <gamboa040200@gmail.com>
2024-05-29 10:01:53 -0600

    donerino

--- src/routes/movements.ts
+++ src/routes/movements.ts
@@ -6,5 +6,6 @@ const router = express.Router();
 router.put('/movements', MovementsRouter.upsertMovementsController);
 router.get('/movements/:individual_id', MovementsRouter.getMovements);
 router.delete('/movements/:movement_id', MovementsRouter.deleteMovementController);
+router.post('/movements/output', MovementsRouter.generateOutputMovement);
 
 export default router;
